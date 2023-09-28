123b50a9ccdd6043c764e709ba6fa8eb78475684
ej-shafran <ejshafran@hilma.tech>
2023-09-28 11:35:23 +0300

    Overhall config

--- bin/i3bar.sh
+++ bin/i3bar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 i3status -c ~/.config/i3/i3status.conf | while :
 do
-    read line
+    read -r line
     network=$(~/.config/i3/bin/network )
     echo "$network | $line" || exit 1
 done
